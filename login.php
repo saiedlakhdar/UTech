665ab4065586c00751bc401e5afa097beb3180ce
saiedlakhdar <saied.lakhdar01@gmail.com>
2017-09-30 00:31:38 +0100

    UTech last update

---
+++ login.php
@@ -0,0 +1,259 @@
+<?php
+/**
+ * Created by SAIED-LOAI-LAKHDAR.
+ * User: DjelfaNetwork
+ * Date: 29-09-2017
+ * Time: 15:01
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>U-Tech | Corporation</title>
+
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="U-Tech">
+
+    <!-- Google Fonts -->
+    <link href='css/font.css' rel='stylesheet'>
+
+    <!-- Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-icons.css" />
+    <link rel="stylesheet" href="css/settings.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+    <link rel="stylesheet" href="css/color.css" />
+    <link rel="stylesheet" href="css/main-rtl.css" />
+    <link rel="stylesheet" href="css/spacings.css" />
+    <link rel="stylesheet" href="css/font-awesome.css" />
+    <link rel="stylesheet" href="css/lo-reg.css" />
+    <link rel="stylesheet" href="vegas/vegas.min.css" />
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="img/logo-250x250.png">
+
+
+</head>
+
+<body data-spy="scroll" data-offset="60" data-target=".navbar-fixed-top">
+
+<!-- Preloader -->
+<div class="loader-mask">
+    <div class="loaders">
+        <div id="inTurnBlurringTextG"><div id="inTurnBlurringTextG_1" class="inTurnBlurringTextG">U</div><div id="inTurnBlurringTextG_2" class="inTurnBlurringTextG">-</div><div id="inTurnBlurringTextG_3" class="inTurnBlurringTextG">T</div><div id="inTurnBlurringTextG_4" class="inTurnBlurringTextG">E</div><div id="inTurnBlurringTextG_5" class="inTurnBlurringTextG">C</div><div id="inTurnBlurringTextG_6" class="inTurnBlurringTextG">H</div><div id="inTurnBlurringTextG_7" class="inTurnBlurringTextG"> </div><div id="inTurnBlurringTextG_8" class="inTurnBlurringTextG">.</div><div id="inTurnBlurringTextG_9" class="inTurnBlurringTextG">.</div><div id="inTurnBlurringTextG_10" class="inTurnBlurringTextG">.</div></div>
+
+    </div>
+
+</div>
+
+<!-- Navigation -->
+<header class="nav-type-1" id="home">
+
+    <nav class="navbar navbar-fixed-top">
+        <div class="navigation-overlay">
+            <div class="container-fluid relative">
+                <div class="row">
+
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <!-- Logo -->
+                        <div class="logo-container">
+                            <div class="logo-wrap local-scroll">
+                                <a href="#home">
+                                    <img class="logo" src="img/logo.png" alt="logo">
+                                </a>
+                            </div>
+                        </div>
+                    </div> <!-- end navbar-header -->
+
+
+                    <div class="col-md-8 col-xs-12 nav-wrap pull-right">
+                        <div class="collapse navbar-collapse text-center" id="navbar-collapse">
+
+                            <ul class="nav navbar-nav local-scroll text-center">
+                                <li>
+                                    <a class="ulista" href="index.php">الرئيسية</a>
+                                </li>
+
+                                <li >
+                                    <a class="ulista" href="register.php">تسجيل <i class="fa fa-user-plus"></i></a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                    </div> <!-- end col -->
+
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </div> <!-- end navigation -->
+    </nav> <!-- end navbar -->
+
+</header>
+<!-- end navigation -->
+<!--[ main Wrapper ]-->
+
+<div class="login-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div id="login-wrap"> </div>
+
+
+        </div>
+    </div>
+</div>
+
+<div class="align">
+            <div class="grid">
+
+                <form class="material">
+                    <div class="group">
+                        <input type="text" required><span class="highlight"></span><span class="bar"></span>
+                        <label>إسم المستخدم</label>
+                    </div>
+                    <div class="group">
+                        <input type="password" required><span class="highlight"></span><span class="bar"></span>
+                        <label>كلمة المرور</label>
+                    </div>
+                    <button type="button" class="button buttonBlue">دخول
+                        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+                    </button>
+
+                        <h3 class="register">ليس ليدك حساب ؟ <a href="register.php">سجل</a></h3>
+
+                </form>
+
+            </div>
+
+</div>
+
+
+<div class="clearfix"></div>
+
+<!--[ main Wrapper ]-->
+<!--[ main Wrapper ]-->
+<!--[last Section ]-->
+<div class="container-fluid" style="background: #242424">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="utech" >
+                    <h2 class="neon h">U-Tech</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+</div>
+<div class="clearfix"></div>
+<footer class="footer minimal bg-dark">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4 col-md-offset-4">
+
+                <div class="footer-logo local-scroll wow zoomIn" data-wow-duration="1s" data-wow-delay="0.2s">
+
+                </div>
+
+                <div class="socials footer-socials">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+
+                </div> <!-- end socials -->
+
+                <span class="copyright text-center">
+              ©2017 U-Tech  |  <a href="http://fb.com/Djelfanetwork">Designer</a>
+            </span>
+
+            </div>
+
+        </div>
+    </div>
+</footer> <!-- end footer -->
+
+<div id="back-to-top">
+    <a href="#top"><i class="fa fa-angle-up"></i></a>
+</div>
+
+<script src="http://zeptojs.com/zepto.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+<script type="text/javascript" src="js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="js/jquery.themepunch.revolution.min.js"></script>
+<script type="text/javascript" src="js/rev-slider.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+<script type="text/javascript" src="vegas/vegas.min.js"></script>
+
+<script type="text/javascript" >
+    //
+    $(window).load(function() {
+
+        // slider background
+        $("#login-wrap,body").vegas({
+            slides: [
+                { src: "img/login1.jpg" },
+                { src: "img/login2.jpg" },
+                { src: "img/login3.jpg" },
+                { src: "img/login4.jpg" }
+            ]
+        });
+
+
+    });
+
+    // Login form
+    $(window, document, undefined).ready(function() {
+
+        $('input').blur(function() {
+            var $this = $(this);
+            if ($this.val())
+                $this.addClass('used');
+            else
+                $this.removeClass('used');
+        });
+
+        var $ripples = $('.ripples');
+
+        $ripples.on('click.Ripples', function(e) {
+
+            var $this = $(this);
+            var $offset = $this.parent().offset();
+            var $circle = $this.find('.ripplesCircle');
+
+            var x = e.pageX - $offset.right;
+            var y = e.pageY - $offset.top;
+
+            $circle.css({
+                top: y + 'px',
+                right: x + 'px'
+            });
+
+            $this.addClass('is-active');
+
+        });
+
+        $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+            $(this).removeClass('is-active');
+        });
+
+    });
+
+</script>
+
+
+</body>
+</html>
